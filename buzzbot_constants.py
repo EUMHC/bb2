1aba3a4165c22549459dd5c56837e7ae8b6f7d4f
Callum Alexander <callumalexander.personal@gmail.com>
2024-06-07 11:20:44 +0100

    feat: limit the number of fixtures that can be added manually to the length of the teams array.
    feat: ensure uni team for manual selection is unique.

--- buzzbot_constants.py
+++ buzzbot_constants.py
@@ -29,8 +29,8 @@ class BuzzBotConfiguration:
 
 
     def validate_file(self):
-        if len(self.settings['distance_matrix_ai']['api_key']) < 20:
-            raise ValueError("DistanceMatrix API not recognised as correct format")
+        if self.settings['distance_matrix_ai']['api_key'] is None or len(self.settings['distance_matrix_ai']['api_key']) < 20:
+            raise ValueError(f"DistanceMatrix API not recognised as correct format. Got {self.settings['distance_matrix_ai']['api_key']} ")
 
     def save(self):
         with open(self.config_file, 'w') as file:
