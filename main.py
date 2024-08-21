262f50421d33cdec77512957f53e6e4296825ba1
Callum Alexander <callumalexander.personal@gmail.com>
2024-08-21 15:26:18 +0100

    refactor

--- main.py
+++ main.py
@@ -4,31 +4,36 @@ import buzzbot_constants
 import gspread_interface
 from buzzbot_constants import buzzbotConfiguration
 
-start_time = time.time()
 
-# TODO: have these as config variables rather than hardcoded.
+def main():
+    total_start_time = time.time()
 
-# manager = gspread_interface.GoogleSheetManager("buzzbot2-2d1f4bb04d1a.json", "Copy of Fixtures Umpiring combo 2022/23")
+    manager = gspread_interface.GoogleSheetManager(
+        credentials_file=buzzbotConfiguration.settings["google_credentials_filename"],
+        sheet_name=buzzbotConfiguration.settings["google_sheet_doc_name"],
+    )
+    matches = manager.read_sheet_as_fixtures("Fixtures List")
 
-print(buzzbotConfiguration.settings["google_sheet_doc_name"])
+    compute_start_time = time.time()
+    teams = buzzbotConfiguration.settings["teams"]
+    umpiring_count = {team: 0 for team in teams}
+    selection_criteria = buzzbot_constants.get_selection_criteria()
+    bot = buzzbot.BuzzBot(matches, teams, umpiring_count, criteria_=selection_criteria)
+    bot.assign_covering_teams(print_results=False)
 
-manager = gspread_interface.GoogleSheetManager(
-    credentials_file=buzzbotConfiguration.settings["google_credentials_filename"],
-    sheet_name=buzzbotConfiguration.settings["google_sheet_doc_name"],
-)
-matches = manager.read_sheet_as_fixtures("Fixtures List")
+    games = bot.matches
+    games.sort(key=lambda x: x.start_time)
+    compute_end_time = time.time()
 
-teams = buzzbotConfiguration.settings["teams"]
-umpiring_count = {team: 0 for team in teams}
-selection_criteria = buzzbot_constants.get_selection_criteria()
-bot = buzzbot.BuzzBot(matches, teams, umpiring_count, criteria_=selection_criteria)
-bot.assign_covering_teams(print_results=False)
+    manager.write_assignments("Assignments", games)
 
-games = bot.matches
-games.sort(key=lambda x: x.start_time)
+    total_end_time = time.time()
+    total_time = total_end_time - total_start_time
+    total_compute_time = compute_end_time - compute_start_time
+    print(
+        f"Total runtime: {total_time:.2f} seconds\nTotal compute time: {total_compute_time:.2f} seconds"
+    )
 
-manager.write_assignments("Assignments", games)
 
-end_time = time.time()
-total_time = end_time - start_time
-print(f"Total runtime: {total_time:.2f} seconds")
+if __name__ == "__main__":
+    main()
